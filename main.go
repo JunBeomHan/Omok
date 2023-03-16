6c0d53a48bc057e81ff28185dd7f442cf3704245
JunBumHan <2hanjunbum6@gmail.com>
2023-03-17 08:40:10 +0900

    Add main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	// board := Board{}
+	// board.initBoard()
+
+	// var row, colum int
+
+	// player1 := Stone{stoneStatus: ONE_PLAYER}
+	// // player2 := Stone{stoneStatus: TWO_PLAYER, row: 0, colum: 0}
+
+	// for {
+	// 	fmt.Scan(&row, &colum)
+	// 	if !board.checkStonePresence(row, colum) {
+	// 		player1.placeMove(&board, row, colum)
+	// 	} else {
+	// 		fmt.Println("이미 있어요!")
+	// 	}
+
+	// 	if board.checkWinner(player1, row, colum) {
+	// 		fmt.Println("WIN!")
+	// 	}
+
+	// 	board.printBoard()
+	// }
+}
